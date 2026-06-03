6ac461da05482f541b8ee9d5d1161b6c9ceedaa9
Arseniy Novitsky <eneizi1616@gmail.com>
2026-06-03 23:43:44 +0400

    add registration disign

--- src/registration.js
+++ src/registration.js
@@ -7,12 +7,21 @@ export function registration() {
     registrationBtn.addEventListener("click", () => {
         const renderMain = document.querySelector("#main-content")
         renderMain.innerHTML = `
-            <section class="registration-container">
-                <button class="back-to-main">
-                    <img src="src/assets/icons/arrow_arrows_back_direction_left_navigation_right.svg" width="35px" height="35px">
-                </button>
-            </section>
-        `
+        <button class="back-to-main">
+            <img src="src/assets/icons/arrow_arrows_back_direction_left_navigation_right.svg" width="35px" height="35px">
+        </button>
+        <section class="registration-container">
+            <div class="registration-block">
+                <label for="login">Введите email:</label>
+                <input class="login-inp" id="login" type="email" placeholder="Логин">
+                <label for="login">Введите логин:</label>
+                <input class="login-inp" id="login" type="text" placeholder="Логин">
+                <label for="login">Введите пароль:</label>
+                <input class="pass-inp" type="password" placeholder="Пароль">
+                <button class="aprove-registration">Подтвердить</button>
+            </div>
+        </section>
+        `;
 
         // кнопка возрата
         const backMain = document.querySelector(".back-to-main")
